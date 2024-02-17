3874280ba65e052d93acb1a53b21bb5844223919
vaibbhavk <vaibhav.vk2128@gmail.com>
2024-02-17 15:34:54 +0530

    Initial commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreditPage = () => {
+  return <div>CreditPage1</div>;
+};
+
+export default CreditPage;
---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return <div>ProfilePage1</div>;
+};
+
+export default ProfilePage;
